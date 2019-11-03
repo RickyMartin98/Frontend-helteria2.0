b2d33cd386b357b065003e5012aaa3266b9d39b3
Ricky <mfernandez2@ucol.mx>
2019-11-03 13:34:47 -0600

    ready to deploy it

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useState,  useEffect } from 'react';
 import Platillo from '../Platillo';
 import platillosTest from './TestData.js';
 import providers from '../Login';
+import firebase from 'firebase';
 
 
 const Menu = function () {
+	const [bebidas, setBebidas] = useState([]);
+
+	useEffect(async () => {
+		const data = firebase.database();
+        let refToData = data.ref("Bebida");
+        let refToPlatillos = data.ref("Platillos");
+        refToData.on("child_added", function ( snapshot) {
+            //console.log("La bebida actual es: ",snapshot.val());
+            console.log(bebidas);
+            setBebidas([...bebidas,snapshot.val()]);
+            console.log("El id actual es: ",snapshot.key);
+        });
+	},[]);
+	
+
+
+
+
 	console.log("PROVIDERS: ",providers);
 	console.log(platillosTest);
 	const { Lunes }  = platillosTest;
